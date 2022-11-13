c06952e76e5b20d0c831a999a301bf98f3aa4b37
ranjeetrai42 <rai.prakash.ranjeet@gmail.com>
2022-11-13 13:07:23 +0530

    First program commit

--- AssignmentsJava/src/in/ineuron/main/DuplicatesInArray.java
+++ AssignmentsJava/src/in/ineuron/main/DuplicatesInArray.java
@@ -7,12 +7,33 @@ public class DuplicatesInArray {
 		int n;
 		@SuppressWarnings("resource")
 		Scanner scan=new Scanner(System.in);
+		System.out.print("Enter the size of the array: ");
 		n=scan.nextInt();
-		int[] arr= new int[n];
+		System.out.println("Enter the array elements");
+		int arr[]=new int[n];
 		for(int i=0;i<n;i++) {
 			arr[i]=scan.nextInt();
 		}
-		System.out.println("Hello World");
+		int arrdup[]=new int[n];
+		int k=0;
+		Arrays.sort(arr);
+		for(int i=1;i<n;i++) {
+			if(arr[i]==arr[i-1]) {
+				if(k==0||arr[k-1]!=arr[i]) {
+					arrdup[k]=arr[i];
+					k++;
+				}
+			}
+		}
+		if(k==0) {
+			System.out.println("No duplicates in the aray");
+		}
+		else {
+			System.out.println("Duplicates are: ");
+			for(int i=0;i<k;i++) {
+				System.out.print(arrdup[i]+" ");
+			}
+		}
 	}
 
 }
